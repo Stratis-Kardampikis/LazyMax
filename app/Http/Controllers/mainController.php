f7bbadc90214fdc098e90f406389cba2fc358346
Stratis-Kardampikis <56542230+Stratis-Kardampikis@users.noreply.github.com>
2021-03-10 17:47:00 +0200

    Update mainController.php

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -13,66 +13,7 @@ use App\Fileupload;
 
 class mainController extends Controller
 { public $save = false;
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request)
     {
        
@@ -97,30 +38,7 @@ class mainController extends Controller
         }
         
    }
-  /* public function updatepassword(Request $request)
-   {
-      
-       $email = $request->email;
-       $new = $request->newuser;
-     
-       $error_message="DENIED";
-       $Success = "Success";
-       $hash = Hash::make($new);
-       if ($user = User::where('username','=' ,$email)->first()) {
-           
-           $user->password= $hash;
-           $user->save();
-       
-           return response()->json(['message' => $user->name]);
-       } else {
-          return response()->json(['message' => $error_message]);
-       }
-       
-  }*/
  
-
- 
-
  
  
   public function upload(Request $request)
@@ -137,14 +55,5 @@ class mainController extends Controller
  }
  
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+ 
 }
